f0db44aec41bd1085b0ffbea695ad3a4c2f56391
Jhon Maiza <jhonmaiza@outlook.com>
2025-04-15 17:39:33 -0500

    fix ImageURLs

--- 08-journal-app/src/firabase/config.js
+++ 08-journal-app/src/firabase/config.js
@@ -2,17 +2,29 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import { getEnviroments } from '../helpers';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID
+
+} = getEnviroments();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDvGbK0mvZT225zDZWsmTAR1IFk7ISN2IQ",
-  authDomain: "react-curso-e5ff3.firebaseapp.com",
-  projectId: "react-curso-e5ff3",
-  storageBucket: "react-curso-e5ff3.firebasestorage.app",
-  messagingSenderId: "1081039451809",
-  appId: "1:1081039451809:web:32714cef0010190c2462e0"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID
 };
 
 // Initialize Firebase
--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -10,6 +10,7 @@ export const startNewNote = () => {
         const newNote = {
             title: '',
             body: '',
+            imageUrls: [],
             date: new Date().getTime(),
         };
 
